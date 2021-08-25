1143f2c664389508baf24aa7f778c1c46fdc35e9
my0990 <my0990@naver.com>
2021-08-25 17:21:15 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Test from './components/test.js'
+
+function App() {
+  return (
+    <div className="App">
+      <Test />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+function Test(){
+
+    const Container = styled.div`
+        width: 100%;
+        background: skyblue;
+    `
+    return (
+        <Container><div>hello</div></Container>
+        
+    )
+}
+
+export default Test;
