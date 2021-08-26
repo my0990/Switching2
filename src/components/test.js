d4644521eef7de0c48432a40d943766cb62a0bf8
my0990 <my0990@naver.com>
2021-08-26 16:14:37 +0900

    thierd commit

--- src/components/test.js
+++ src/components/test.js
@@ -1,16 +1,87 @@
-import React from 'react';
+import React,{useState} from 'react';
 import styled from 'styled-components';
 
 
-function Test(){
 
-    const Container = styled.div`
+const SwitchingContainer = styled.div`
         width: 100%;
         background: skyblue;
     `
+
+const BtnWrapper = styled.div`
+    width: 1100px;
+    background: white;
+    margin: 0 auto;
+`
+
+const DeskWrapper = styled.div`
+    width: 1100px;
+    background: white;
+    margin: 0 auto;
+    
+    
+    
+`
+const DeskStyle = styled.div`
+    width: 100px;
+    height: 50px;
+    background: skyblue;
+    border-radius: 5px;
+    
+    
+`
+
+function Test(){
+
+    
+
+    const [col,setCol] = useState(0);
+    const [row,setRow] = useState(0);
+    const colArr = [...Array(parseInt(col))]
+    const rowArr = [...Array(parseInt(row))]
+    
+    
+
     return (
-        <Container><div>hello</div></Container>
         
+        <SwitchingContainer>
+            <BtnWrapper >
+                <div>hello</div>
+                <label>Col</label>
+                <input onChange={(e)=>{setCol(e.target.value)}}></input>
+                <label>Row</label>
+                <input onChange={(e)=>{setRow(e.target.value)}}></input>
+                <button>입력</button>
+                <button onClick={()=>{console.log(colArr)}}>test</button>
+            </BtnWrapper>
+            <DeskWrapper>
+                <table>
+                {colArr.map((a,i)=> {
+                    return(
+                        <tr>
+                        {rowArr.map((a,i)=>{
+                            return(
+                                <td><Desk>a</Desk></td>
+                            )
+                        })}
+                        </tr>
+                    )
+                })}
+                </table>
+            </DeskWrapper>
+            
+        </SwitchingContainer>
+        
+    )
+}
+
+const Desk = ({children}) => {
+
+    
+    return(
+        <DeskStyle>
+            {children}
+        </DeskStyle>
     )
 }
 
